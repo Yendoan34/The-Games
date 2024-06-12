9284d4433268366f2e2d9ab1df903761d7cc6b93
Yendoan34 <156996880+Yendoan34@users.noreply.github.com>
2024-06-12 13:36:12 +0300

    Make story scene

---
+++ Assets/Scripts/NextButton1.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextButton1 : MonoBehaviour
+{
+    public GameObject[] objectsToToggle; // Array of GameObjects to toggle
+    public Animator barbie;
+    public Animator ken;
+    public Animator fairy;
+    private int currentIndex = 0; // Index of the currently active object
+    public void Start()
+    {
+        barbie.SetTrigger("Sad Talk");
+    }
+    public void OnNextButtonClick()
+    {
+        // If there are objects left to toggle
+        if (currentIndex < objectsToToggle.Length-1)
+        {
+            // Disable the current object
+            objectsToToggle[currentIndex].SetActive(false);
+            // Move to the next object
+            currentIndex++;
+            // Enable the next object
+            objectsToToggle[currentIndex].SetActive(true);
+            if (currentIndex == 7)
+            {
+                ken.SetTrigger("Ken Sad Talk");
+            }
+            else if (currentIndex%2 == 0)
+            {
+                barbie.SetTrigger("Sad Talk");
+            }
+            else
+            {
+                fairy.SetTrigger("Fairy Talk");
+            }
+        }
+        else
+        {
+            LoadScene();
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
---
+++ Assets/Scripts/Story2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Story2 : MonoBehaviour
+{
+    public GameObject thunders1;
+    public GameObject thunders2;
+    public GameObject thunders3;
+    public GameObject fairyObject;
+    public Animator fairy;
+    public GameObject teddyObject;
+    public Animator teddy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Thunder());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Thunder()
+    {
+        yield return new WaitForSeconds(1.5f);
+        thunders1.SetActive(true);
+        fairyObject.SetActive(true);
+        fairy.SetTrigger("Fairy Move");
+        yield return new WaitForSeconds(1.5f);
+        thunders1.SetActive(false);
+        thunders2.SetActive(true);
+        teddyObject.SetActive(true);
+        teddy.SetTrigger("Teddy Move");
+        yield return new WaitForSeconds(1.5f);
+        thunders2.SetActive(false);
+        thunders3.SetActive(true);
+    }
+}
