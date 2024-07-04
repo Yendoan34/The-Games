4b7e7c1eb7f8752e7b491f48cdcce9a1d62dc544
Yendoan34 <156996880+Yendoan34@users.noreply.github.com>
2024-07-04 11:05:49 +0300

    Fix button click time with no target bug

--- Assets/Scripts/LimitedButtonClick.cs
+++ Assets/Scripts/LimitedButtonClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 // Script to limit the time to click the button has this script
@@ -11,6 +12,9 @@ public class LimitedButtonClick : MonoBehaviour
     private Button button;
     private int time; // amount of time that can click, show at the button
     TextMeshProUGUI timeText;
+    public GameObject noTarget;
+    private bool hasUpdated = false;
+    private static LimitedButtonClick lastClickedButton; // Static reference to the last clicked button
     void Start()
     {
         time = maxClicks - clickCount; // Get the amount at the beginning
@@ -19,15 +23,34 @@ public class LimitedButtonClick : MonoBehaviour
         button = GetComponent<Button>(); // Get the Button component
         button.onClick.AddListener(OnButtonClick); // Add a click event to track when the button is clicked
     }
-
+    private void Update()
+    {
+        if (lastClickedButton == this && noTarget.activeSelf && !hasUpdated)
+        {
+            if (clickCount >= maxClicks)
+            {
+                this.button.interactable = true;
+            }
+            clickCount--; // Disable the last click because cannot find target
+            time = maxClicks - clickCount; // Update the time amount
+            timeText.text = $"{time}"; // Update the text
+            hasUpdated = true;
+        }
+        // Reset the flag if the GameObject is disabled
+        if (!noTarget.activeSelf)
+        {
+            hasUpdated = false;
+        }
+    }
     void OnButtonClick()
     {
+        lastClickedButton = this; // Set this button as the last clicked button
         clickCount++; // Increase 1 when button is clicked
         time = maxClicks - clickCount; // Update the time amount after clicked 1 time
         timeText.text = $"{time}"; // Update the text
         if (clickCount >= maxClicks)
         {
-            button.gameObject.SetActive(false); // Disable the button if run out of time
+            this.button.interactable = false;
         }
     }
 }
